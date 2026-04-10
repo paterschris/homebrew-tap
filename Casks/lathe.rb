e1e362c40e5c3adcf1aa968458b7c07e35d0a3a3
Chris Paterson <chrisp@tommycarwash.com>
2026-04-10 14:45:59 -0400

    Update Lathe v0.233.1 SHA for signed build

--- Casks/lathe.rb
+++ Casks/lathe.rb
@@ -1,6 +1,6 @@
 cask "lathe" do
   version "0.233.1"
-  sha256 "ed62bb58917cf302a0c3e63fb251a32d77f86624c4ccf7188112b58df235c78a"
+  sha256 "36f091cca9d8f60ca72910531e26e7710383f41d4a01c659012d65584f2443ce"
 
   url "https://github.com/paterschris/lathe/releases/download/v#{version}/Lathe-#{version}-aarch64-macos.zip"
   name "Lathe"
